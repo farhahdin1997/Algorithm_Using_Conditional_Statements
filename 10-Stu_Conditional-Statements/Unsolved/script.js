53632a41ca9ca2fc8d914e630c2a79f9249d6d49
farhah din <farhahdin97@hotmail.com>
2021-12-14 19:47:53 +0000

    statements are met

---
+++ 10-Stu_Conditional-Statements/Unsolved/script.js
@@ -0,0 +1,18 @@
+// Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = (x < 25);
+var expression2 = (x > 50);
+
+// Write Your JavaScript Code Here
+console.log(x >25)
+console.log(x === 50)
+
+
+console.log(x >25)
+console.log(x === 40)
+
+console.log(x < 25)
+console.log(x === 50)
+
+console.log(x < 25)
+console.log(x === 40)
